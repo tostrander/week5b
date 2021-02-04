6b7775b65d6b4e9bf0381964f0e576ba390bee07
Tina Ostrander <tostrander@greenriver.edu>
2021-02-04 14:17:40 -0500

    Initial setup

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once('vendor/autoload.php');
+
+//Instantiate Fat-Free
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function() {
+
+    echo "Home page";
+    //Display a view
+    //$view = new Template();
+    //echo $view->render('views/home.html');
+});
+
+//Run Fat-Free
+$f3->run();
